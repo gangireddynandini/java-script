28c143d4fb8706b8ba98c9592b38ef4bbb240b5c
gangireddynandini <123799756+gangireddynandini@users.noreply.github.com>
2023-05-12 18:24:05 +0530

    Add files via upload

---
+++ fun4.js
@@ -0,0 +1,28 @@
+// function add(a, b) {
+//     var dh = typeof b !== "undefined" ? b : 1;
+//     return a * b
+
+// }
+// console.log(add(4, 9));
+console.log('-----------------------------------------------------------');
+// a = ['hydrozen', 'lithium', 'water', 'carb']
+// const formla = a.map(function (s) {
+//     return s.length;
+// })
+// console.log(formla)
+console.log('------------------------------------------------------------------')
+let a = {
+    firstname: 'gangireddy',
+    lastname: "nandini",
+    course: 'python',
+    college: 'sv arts and science',
+    fust: function () {
+        console.log(`${a.college.split(' ')[0]}`)
+
+    }
+}
+a.fust();
+console.log(`first name ${a.firstname}`)
+
+
+
